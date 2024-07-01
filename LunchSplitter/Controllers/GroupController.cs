880c1816c83cb308f88264ad56e6ec3810e077a5
danielmoss <121039995+danielmosss@users.noreply.github.com>
2024-07-01 09:23:24 +0200

    push

---
+++ LunchSplitter/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using LunchSplitter.Data;
+using LunchSplitter.Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+namespace LunchSplitter.Controllers;
+
+public class GroupController : ControllerBase
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public GroupController(DatabaseContext databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    // get groups
+    public List<Group> GetGroups()
+    {
+        return _databaseContext.Groups.ToList();
+    }
+}
