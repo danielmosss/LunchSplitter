4a9b34f9dedcca2c2f9a6ea54c2c160f8f2b624d
danielmoss <121039995+danielmosss@users.noreply.github.com>
2024-07-02 11:16:36 +0200

    grouppage working

--- LunchSplitter/Services/GroupService.cs
+++ LunchSplitter/Services/GroupService.cs
@@ -16,14 +16,57 @@ public class GroupService
     public List<Group> GetGroups()
     {
         using var context = _dbContextFactory.CreateDbContext();
-        return context.Groups.ToList();
+        var groups = context.Groups.Include(g => g.GroupUsers).ThenInclude(gu => gu.User).ToList();
+        return groups;
     }
 
-    public async void AddGroup(Group group)
+    public Group GetGroupById(int groupId)
+    {
+        using (var context = _dbContextFactory.CreateDbContext())
+        {
+            var group = context.Groups.Include(g => g.GroupUsers).ThenInclude(gu => gu.User).FirstOrDefault(g => g.Id == groupId);
+            return group;
+        }
+    }
+    
+    public async void DeleteGroup(int groupId)
+    {
+        Console.WriteLine("Deleting group with id: " + groupId);
+        using (var context = _dbContextFactory.CreateDbContext())
+        {
+            Group group = context.Groups.Find(groupId);
+            context.Groups.Remove(group);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public async void AddGroup(Group group, int userId)
     {
         using (var context = _dbContextFactory.CreateDbContext())
         {
             context.Groups.Add(group);
+            await context.SaveChangesAsync();
+            
+            AddUserToGroup(group, userId);
+        }
+    }
+    
+    public async void AddUserToGroup(Group group, int userId)
+    {
+        using (var context = _dbContextFactory.CreateDbContext())
+        {
+            User user = context.Users.Find(userId);
+            Group dbGroup = context.Groups.Find(group.Id);
+            if (dbGroup.GroupUsers == null)
+            {
+                dbGroup.GroupUsers = new List<GroupUser>();
+            }
+            GroupUser groupUser = new GroupUser
+            {
+                Group = dbGroup,
+                User = user
+            };
+            dbGroup.GroupUsers.Add(groupUser);
             await context.SaveChangesAsync();
         }
     }
