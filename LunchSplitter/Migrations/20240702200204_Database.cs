b964c0561b6d6766c1f0c39535ee49393012eff1
danielmoss <121039995+danielmosss@users.noreply.github.com>
2024-07-03 06:53:41 +0200

    added gitignore and migration.

---
+++ LunchSplitter/Migrations/20240702200204_Database.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LunchSplitter.Migrations
+{
+    /// <inheritdoc />
+    public partial class Database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    term = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    salt = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupInvites",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    group_id = table.Column<int>(type: "int", nullable: false),
+                    usage = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupInvites", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_GroupInvites_Groups_group_id",
+                        column: x => x.group_id,
+                        principalTable: "Groups",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupUserPermissions",
+                columns: table => new
+                {
+                    group_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    permission_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_GroupUserPermissions_Groups_group_id",
+                        column: x => x.group_id,
+                        principalTable: "Groups",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUserPermissions_Permissions_permission_id",
+                        column: x => x.permission_id,
+                        principalTable: "Permissions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUserPermissions_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupUsers",
+                columns: table => new
+                {
+                    group_user_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    group_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    is_admin = table.Column<bool>(type: "bit", nullable: false),
+                    share = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupUsers", x => x.group_user_id);
+                    table.ForeignKey(
+                        name: "FK_GroupUsers_Groups_group_id",
+                        column: x => x.group_id,
+                        principalTable: "Groups",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUsers_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    group_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Groups_group_id",
+                        column: x => x.group_id,
+                        principalTable: "Groups",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionShares",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    transaction_id = table.Column<int>(type: "int", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    user_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionShares", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_TransactionShares_Transactions_transaction_id",
+                        column: x => x.transaction_id,
+                        principalTable: "Transactions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "id", "email", "name", "password", "salt" },
+                values: new object[] { 1, "admin@example.com", "SystemAdmin", "UCV6fNSmRMhWLiGfMC5L+VeX2qEdw+Rxm65wOSiqSeg=", "usrIE7iR2QtKgIWwfyOiIw==" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupInvites_group_id",
+                table: "GroupInvites",
+                column: "group_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUserPermissions_group_id",
+                table: "GroupUserPermissions",
+                column: "group_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUserPermissions_permission_id",
+                table: "GroupUserPermissions",
+                column: "permission_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUserPermissions_user_id",
+                table: "GroupUserPermissions",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUsers_group_id_user_id",
+                table: "GroupUsers",
+                columns: new[] { "group_id", "user_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUsers_user_id",
+                table: "GroupUsers",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_group_id",
+                table: "Transactions",
+                column: "group_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_user_id",
+                table: "Transactions",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionShares_transaction_id_user_id",
+                table: "TransactionShares",
+                columns: new[] { "transaction_id", "user_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_name",
+                table: "Users",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupInvites");
+
+            migrationBuilder.DropTable(
+                name: "GroupUserPermissions");
+
+            migrationBuilder.DropTable(
+                name: "GroupUsers");
+
+            migrationBuilder.DropTable(
+                name: "TransactionShares");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
